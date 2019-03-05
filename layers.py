c659508207604dd906117c2f700aa970023a42b8
ZhengNie <alongstar518@hotmail.com>
2019-03-05 10:31:26 -0800

    add pe

--- layers.py
+++ layers.py
@@ -54,6 +54,32 @@ class EmbeddingTransformer(nn.Module):
 
         return emb
 
+class PositionalEncoder(nn.Module):
+    def __init__(self, d_model, max_seq_len=0):
+        super().__init__()
+        self.d_model = d_model
+
+        # create constant 'pe' matrix with values dependant on
+        # pos and i
+        pe = torch.zeros(max_seq_len, d_model)
+        for pos in range(max_seq_len):
+            for i in range(0, d_model, 2):
+                pe[pos, i] = \
+                    math.sin(pos / (10000 ** ((2 * i) / d_model)))
+                pe[pos, i + 1] = \
+                    math.cos(pos / (10000 ** ((2 * (i + 1)) / d_model)))
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        # make embeddings relatively larger
+        x = x * math.sqrt(self.d_model)
+        # add constant to embedding
+        seq_len = x.size(1)
+        x = x + torch.autograd.Variable(self.pe[:, :seq_len], requires_grad=False).to(x.device)
+        return x
+
 
 class HighwayEncoder(nn.Module):
     """Encode an input sequence using a highway network.
--- models.py
+++ models.py
@@ -27,7 +27,7 @@ class BiDAF(nn.Module):
         hidden_size (int): Number of features in the hidden state at each layer.
         drop_prob (float): Dropout probability.
     """
-    def __init__(self, word_vectors, hidden_size, drop_prob=0.):
+    def __init__(self, word_vectors, hidden_size, max_p_len, max_q_len,drop_prob=0.):
         super(BiDAF, self).__init__()
         self.emb = layers.Embedding(word_vectors=word_vectors,
                                     hidden_size=hidden_size,
@@ -65,7 +65,8 @@ class BiDAF(nn.Module):
         #                             num_layers=2,
         #                             drop_prob=drop_prob)
 
-
+        self.pe_p = layers.PositionalEncoder(word_vectors.size(-1), max_p_len)
+        self.pe_q = layers.PositionalEncoder(word_vectors.size(-1), max_q_len)
         self.out = layers.BiDAFOutput(hidden_size=hidden_size,
                                       drop_prob=drop_prob)
 
@@ -82,7 +83,8 @@ class BiDAF(nn.Module):
 
         #c_enc = self.enc(c_emb, c_len)    # (batch_size, c_len, 2 * hidden_size)
         #q_enc = self.enc(q_emb, q_len)    # (batch_size, q_len, 2 * hidden_size)
-
+        c_emb = self.pe_p(c_emb)
+        q_emb = self.pe_q(q_emb)
         c_enc = self.enc_trans(c_emb,c_mask)
         q_enc = self.enc_trans(q_emb,q_mask)
 
