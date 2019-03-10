725da72107e2c7e8086e1b85bbd4d7ebc6710581
ZhengNie <alongstar518@hotmail.com>
2019-03-10 04:00:06 -0700

    fix eval

--- train.py
+++ train.py
@@ -172,7 +172,7 @@ def evaluate(model, data_loader, device, eval_file, max_len, use_squad_v2):
             batch_size = cw_idxs.size(0)
 
             # Forward
-            log_p1, log_p2 = model(cw_idxs, qw_idxs)
+            log_p1, log_p2 = model(cw_idxs, qw_idxs, cc_idxs, qc_idxs)
             y1, y2 = y1.to(device), y2.to(device)
             loss = F.nll_loss(log_p1, y1) + F.nll_loss(log_p2, y2)
             nll_meter.update(loss.item(), batch_size)
