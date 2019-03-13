aa2dbfb19b88348d8de5df32c9532c7a39c9e84d
ZhengNie <alongstar518@hotmail.com>
2019-03-12 20:02:11 -0700

    no mask

--- models.py
+++ models.py
@@ -70,8 +70,8 @@ class BiDAF(nn.Module):
         c_emb = self.emb(cw_idxs, cw_char_idx)         # (batch_size, c_len, hidden_size)
         q_emb = self.emb(qw_idxs, qw_char_idxs)         # (batch_size, q_len, hidden_size)
 
-        c_enc = self.enc_trans(c_emb, c_mask)    # (batch_size, c_len, 2 * hidden_size)
-        q_enc = self.enc_trans(q_emb, q_mask)    # (batch_size, q_len, 2 * hidden_size)
+        c_enc = self.enc_trans(c_emb, None)    # (batch_size, c_len, 2 * hidden_size)
+        q_enc = self.enc_trans(q_emb, None)    # (batch_size, q_len, 2 * hidden_size)
 
         att = self.att(c_enc, q_enc,
                        c_mask, q_mask)    # (batch_size, c_len, 8 * hidden_size)
